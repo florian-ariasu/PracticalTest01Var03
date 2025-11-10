3ed3ae4fcd6886dabdb88c3a343daad478e09cdb
florian-ariasu <ariasu.florian@gmail.com>
2025-11-10 11:33:59 +0200

    B1 without the toast display

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var03/PracticalTest01Var03MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var03/PracticalTest01Var03MainActivity.java
@@ -1,24 +1,73 @@
 package ro.pub.cs.systems.eim.practicaltest01var03;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import ro.pub.cs.systems.eim.practicaltest01var03.R;
 
 public class PracticalTest01Var03MainActivity extends AppCompatActivity {
+    private EditText edit_text_1;
+    private EditText edit_text_2;
+    private Button button_plus, button_minus;
+    private TextView result;
+    private PlusButtonListener plus_button = new PlusButtonListener();
+    private MinusButtonListener minus_button = new MinusButtonListener();
+
+
+    private class PlusButtonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            String first_operand = edit_text_1.getText().toString();
+            int first_op = Integer.parseInt(first_operand);
+            String second_operand = edit_text_2.getText().toString();
+            int second_op = Integer.parseInt(second_operand);
+            int op = first_op + second_op;
+            result.setText(op);
+        }
+    }
+
+    private class MinusButtonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            String first_operand = edit_text_1.getText().toString();
+            int first_op = Integer.parseInt(first_operand);
+            String second_operand = edit_text_2.getText().toString();
+            int second_op = Integer.parseInt(second_operand);
+            int op = first_op - second_op;
+            result.setText(op);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_practical_test01_var03_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Initialize UI elements
+        Button plus_button = (Button)findViewById(R.id.btn_1);
+        Button minus_button = (Button)findViewById(R.id.btn_2);
+
+
+        // Set listeners for plus and minus buttons
+        View.OnClickListener PlusButtonListener = null;
+        View.OnClickListener MinusButtonListener = null;
+        plus_button.setOnClickListener(PlusButtonListener);
+        minus_button.setOnClickListener(MinusButtonListener);
     }
 }
